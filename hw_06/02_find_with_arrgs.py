eec9f2f7f669fba6c28fcf224c209eb3b6ce92e3
Chegevarich <chegevarich@gmail.com>
2015-10-27 23:12:12 +0300

    updated&improved

--- hw_06/02_find_with_arrgs.py
+++ hw_06/02_find_with_arrgs.py
@@ -5,15 +5,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--type', default='.py')
 parser.add_argument('-f', '--folder', default='./')
 parser.add_argument('-s', '--substr', default='def')
+parser.add_argument('-r', '--recursive', action='store_const', const=True)
+parser.add_argument('-pd', '--printDecore', action='store_true', default=False)
 
 p = parser.parse_args()
 
-def find(d, t):
+def decorator(func):
+	def decorated(f, t, r, s):
+		if p.printDecore:
+			return func(f, t, r, s, True)
+		else:
+			return func(f, t, r, s)
+
+	return decorated
+
+def find(d, t, r):
 
 	for i in os.listdir(d):
 
-		if os.path.isdir(d+'/'+i) == True:
-			yield from find(d+i+'/', t)
+		if os.path.isdir(d+'/'+i) == True and r == True:
+			yield from find(d+i+'/', t, r)
 		else :
 			if (i.endswith(t)):
 				for fOrDir, line in enumerate(open(d+i)):
@@ -24,5 +35,12 @@ def grep(gen, substr):
 		if substr in s:
 			yield name, i, s
 
-for name, i, s in grep( find(p.folder, p.type) , p.substr):
-	print(name, i, s)
+@decorator
+def grefind(f, t, r, s, decorate=False):
+	for name, i, s in grep( find(f, t, r) , s):
+		if decorate == False:
+			print(name, i, s)
+		else:
+			print('\t', 'Имя файла >>> ', name, '\n\t\t', 'Номер строки >>> ', i, '\n\t\t', 'Вхождение >>> ', s)
+
+grefind(p.folder, p.type, p.recursive, p.substr)
