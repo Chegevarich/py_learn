5375cecf8b066d092ae0c4aff3bb38f607247d03
Chegevarich <chegevarich@gmail.com>
2015-10-29 23:17:11 +0300

    deepKill mode

--- cw_00/process/control.py
+++ cw_00/process/control.py
@@ -1,15 +1,38 @@
-import os, sys
+import os, sys, time
 
-if len(sys.argv) == 3:
+if len(sys.argv) >= 3:
 
 	if sys.argv[1] == 'stop':
 
-		try: #try to kill
-			f = open(sys.argv[2]+'.die', 'w')
-			f.write('1')
-			f.close()
-		except:
-			print('что то пошло не так')
+		#try: #try to kill
+
+			#проверяем надо ли убивать процесс (есть ли у него статус 1)
+			if (open(sys.argv[2]+'.status').read()=='1'):
+
+				while open(sys.argv[2]+'.status').read()=='1':
+					#пишем команду на выключение для скрипта 
+					f = open(sys.argv[2]+'.die', 'w')
+					f.write('1')
+					f.close()
+					#после отправки команды на убийство пишем в статус процесса 0
+					#процесс выключен
+					f = open(sys.argv[2]+'.status', 'w')
+					f.write('0')
+					f.close()
+
+
+					if (len(sys.argv)==4 and sys.argv[3]=='deepKill'):
+						#ждём время выполнения скрипта *2 что бы гарантировать 
+						#попадание запущенных скриптов в этап записи своего статуса
+						#и определения необходимости убийства данных скриптов
+						time.sleep(2)
+
+			else:
+				print('данный скрипт не запущен в рамках текущей псевдоэкосистемы')
+
+
+		#except:
+			#print('что то пошло не так')
 	else:
 		print('не очевидно что может сюда привести')
 else:
