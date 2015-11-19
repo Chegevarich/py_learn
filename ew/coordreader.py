4f0b22676681530105b0fb1394896f06492388c9
Chegevarich <chegevarich@gmail.com>
2015-11-19 22:23:15 +0300

    edc

--- ew/coordreader.py
+++ ew/coordreader.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from abc import ABCMeta, abstractmethod
-from time import sleep
+from time import sleep, time
+from datetime import datetime
 from supercoordreader import SuperBody
 import os, sys
 import xml.etree.ElementTree as ET
 
-class DataProvider():
+class DataProvider(SuperBody):
 
 	#чтение из некоторого источника данных
 	#в целом для переопределения и замены 
@@ -20,19 +21,50 @@ class DataProvider():
 		self.xml = ET.fromstring(str(open(self.source).read()))
 
 	def take_all_coords_by_event_from_xml(self):
+		#все эвенты из подгруженного файла
 		events = self.xml.findall('event')
+
+		#ищем в эвентах эвент с нужным кодом
 		for e in events:
+			#если код совпадает с искомым евентом
 			if e.get('code') == self.event:
 				event = e
+
+		#если получили эвент - запоминаем эвент
 		if e:
 			self.event_data = e
 		else:
 			return False
 
 	def coords_to_dict_from_xml(self):
-		for e in self.event_data:
-			for i in e.iter('coord'):
-				print(i.get('lng'),i.get('lnt'), i.get('time'))
+
+		#перебираем юниты
+		for u in self.event_data:
+			#
+			for c in u.iter('coord'):
+				#print(u.tag)
+
+				if u.get('unit_code') not in self.coord_array:
+					self.coord_array[u.get('unit_code')] = []
+
+				self.coord_array[u.get('unit_code')].append(  [c.get('lng'), c.get('lnt'), c.get('time')]  )
+				
+		for i in self.coord_array:
+			self.coord_array[i].sort(key=lambda x: x[2])
+			print(self.coord_array[i])
+
+
+	def make_start_time(self):
+		self.start_time = int(time())
+		
+		#перебираем каждый юнит
+		for u in self.coord_array:
+			#у каждого юнита рассматриваем геоданные
+			for i in self.coord_array[u]:
+				if int(self.start_time) > int(i[2]):
+					self.start_time = int(i[2])
+			
+
 	#сбор всех координат в единую переменную 
 	#координаты стоит вернуть следующим форматом
 	#dict time => [lng, lnt]
@@ -53,31 +85,70 @@ class DataProvider():
 	#возможно классом для чтения данны либо либо
 	
 	def __init__(self, event, timestep, source):
+		self.coord_array = {}
+		self.coords_by_time_dict = {}
+
 		self.time = timestep
 		self.event = event
 		#self.coords = self.take_all_coords_by_event(self, event)
 		self.source = source
+		self.current_tume = 0
+		
+
 	#возвращение по из общего пулла координат
 	#набор координат за держкой по времени 
 	#координаты привязаны к времени time
-	
 	def coords(self, time):
 		sleep(self.time)
+
+		for u in self.coord_array:
+			#у каждого юнита рассматриваем геоданные
+			for i in self.coord_array[u]:
+				if int(self.start_time) > int(i[2]):
+					self.start_time = int(i[2])
+
+		self.current_tume +=1
+
 		return self.coords[time]
+
+	#формируем список для наблюдения в разрезе времени 
+	def coords_by_time(self):
+		for u in self.coord_array:
+			for i in self.coord_array[u]:
+				if i[2] not in self.coords_by_time_dict:
+					self.coords_by_time_dict[i[2]] = []
+
+				self.coords_by_time_dict[i[2]].append([u, i])
+					
 
 	#запуск чтения ?
 	def coordsreader(self):
 		for i in self.coords:
 			self.coord(i)
 
+	def test(self):
+		print(self.coords_by_time_dict)
+
 DP = DataProvider('first_sample_event', 1, './sample_data/sample_event0.xml')
 
 
-#TODO make method
-DP.read_data_from_xml()
-DP.take_all_coords_by_event_from_xml()
-DP.coords_to_dict_from_xml()
+#TODO make method -> xml_init
 
+#load xml from file
+DP.read_data_from_xml()
+
+#parse typical xml
+DP.take_all_coords_by_event_from_xml()
+
+#make dict [unit] => [ [lnt, lng, time], [ lnt, lng, time] ]
+DP.coords_to_dict_from_xml()
+#time from 
+DP.make_start_time()
+
+#reader by time
+DP.coords_by_time()
+
+DP.test()
 #TODO end of make method
 
 #DP.take_all_coords_by_event_from_xml()
