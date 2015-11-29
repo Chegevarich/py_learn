eaf0748c0e8c75034f3a47a850fd73d6bb594f61
Chegevarich <chegevarich@gmail.com>
2015-11-29 12:08:24 +0300

    ltl

--- cw_07/tst_cw_tv/tst.py
+++ cw_07/tst_cw_tv/tst.py
@@ -1,20 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 
-
 class Tst(Widget):
-
     pass
 
-
 class TstApp(App):
-
     def build(self):
         tst = Tst()
         print(tst.size)
         return tst
-
-
 
 if __name__ == '__main__':
     TstApp().run()
